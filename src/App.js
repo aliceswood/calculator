a30c55d6c4bc1900feb097c917293d077efda1a0
Alice Wood <125405269+aliceswood@users.noreply.github.com>
2024-05-31 11:12:57 +0100

    html& CSS set up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,36 @@
+import React from "react";
+import "./App.css";
 
 function App() {
-	return <div>Placeholder for calculator</div>;
+
+  const numberButtons = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, "."
+  ]
+
+  const operationKeys = [
+    "÷", "x", "-", "+", "="
+  ]
+	return (
+		<div className="calculator-container">
+			<div className="display">
+        <div className="calculation">
+        Placeholder for calculation
+        </div>
+        <div className="input-output">
+        Placeholder for input/output
+        </div>
+      </div>
+			<button className="reset-button">
+        AC
+      </button>
+      {numberButtons.map((number) => {
+        return (<button className="number-button">{number}</button>)
+      })}
+      {operationKeys.map((number) => {
+        return (<button className="operation-button">{number}</button>)
+      })}
+		</div>
+	);
 }
 
 export default App;
