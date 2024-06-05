8f465dbe61c4817566aaaeb4b75f477a4c142aab
Alice Wood <125405269+aliceswood@users.noreply.github.com>
2024-06-05 11:37:33 +0100

    digits add to the display test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,14 @@ import { render, screen, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import App from "./App.js";
 
+const buttons = [
+	"AC", "÷",
+	7, 8, 9, "x", 
+	4, 5, 6, "-", 
+	1, 2, 3, "+", 
+	0, ".", "="
+];
+
 describe("Calculator", () => {
 	it("renders correctly", () => {
 		render(<App />);
@@ -10,25 +18,22 @@ describe("Calculator", () => {
 		expect(screen.getByTestId('previous-calculation')).toBeInTheDocument();
 		expect(screen.getByTestId('current-calculation')).toBeInTheDocument();
 
-		const buttons = [
-      "AC", "÷",
-      7, 8, 9, "x", 
-      4, 5, 6, "-", 
-      1, 2, 3, "+", 
-      0, ".", "="
-    ];
-
-		buttons.forEach((button) => {
+			buttons.forEach((button) => {
 			expect(screen.getByText(button)).toBeInTheDocument();
 		});
 	});
 
-	xit('adds correctly', () => {
+	it('adds digits to the display correctly', () => {
 		render(<App />);
+		const display = screen.getByTestId("display");
 
+		fireEvent.click(screen.getByText('1'));
+		fireEvent.click(screen.getByText('2'));
+		fireEvent.click(screen.getByText('3'));
 		
-
+		expect(display).toHaveTextContent("123");
 	})
+
 
 	it('clears the display', () => {
 		render(<App />);
